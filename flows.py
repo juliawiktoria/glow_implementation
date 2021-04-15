99d70b6d58125a9e90e6d0aecdeb1793460ae421
juliawiktoria <maybe.nott123@gmail.com>
2021-04-15 11:25:40 +0100

    imports

---
+++ flows.py
@@ -0,0 +1,14 @@
+# standard
+import numpy as np
+
+# pytorch
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.data as data
+import torch.optim as optim
+import torch.optim.lr_scheduler as sched
+import torch.backends.cudnn as cudnn
+
+# my functions
+from utilities import mean_over_dimensions
