495826a2462944452fc4841c6ea66e176583b060
juliawiktoria <maybe.nott123@gmail.com>
2021-04-20 17:09:30 +0100

    imports

--- flows.py
+++ flows.py
@@ -1,5 +1,7 @@
 # standard
 import numpy as np
+import matplotlib.pyplot as plt
+from scipy import linalg as splin
 
 # pytorch
 import torch
@@ -9,7 +11,11 @@ import torch.utils.data as data
 import torch.optim as optim
 import torch.optim.lr_scheduler as sched
 import torch.backends.cudnn as cudnn
+import torch.distributions as distrib
+import torch.distributions.transforms as transform
 
 # my functions
 from utilities import mean_over_dimensions
+
+
 
