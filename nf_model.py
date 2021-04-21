afec7e53a8905222e7f71abc23c8dd17cff8d0e6
juliawiktoria <maybe.nott123@gmail.com>
2021-04-21 15:38:45 +0100

    added simple nf model

--- nf_model.py
+++ nf_model.py
@@ -9,6 +9,8 @@ import torch.utils.data as data
 import torch.optim as optim
 import torch.optim.lr_scheduler as sched
 import torch.backends.cudnn as cudnn
+import torch.distributions as distrib
+import torch.distributions.transforms as transform
 
 from model_parts import ActivationNormalisation, AffineCoupling, InvertedConvolution
 from utilities import squeeze
@@ -16,3 +18,26 @@ from utilities import squeeze
 class Block(nn.Module):
     def __init__(self):
         super().__init__()
+
+
+class NormalisingFlow(nn.Module):
+    def __init__(self, dimension, flow_block, num_blocks, density):
+        super().__init__()
+        flows = []
+        for block in num_blocks:
+            for flow in flow_block:
+                flows.append(flow(dimension))
+        self.flows = nn.ModuleList(flows)
+        self.transforms = transform.ComposeTransform(flows)
+        self.base_density = density
+        self.final_density = distrib.TransformedDistribution(density, self.transforms)
+        self.log_det = []
+
+    def forward(self, z):
+        self.log_det = []
+
+        # apply series of flows
+        for i in range(len(self.flows)):
+            self.log_det.append(self.flows[i].log_abs_det_jacobian(z))
+            z = self.flows[i](z)
+        return z, self.log_det
