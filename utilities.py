89c72848cc4f665f07e12f259742793f5fd26b78
juliawiktoria <maybe.nott123@gmail.com>
2021-04-24 00:06:29 +0100

    gaussian functions

--- utilities.py
+++ utilities.py
@@ -1,4 +1,5 @@
 import numpy as np
+import math
 import torch
 import torch.nn.utils as utils
 import os
@@ -43,7 +44,6 @@ class NLLLoss(nn.Module):
             - np.log(self.k) * np.prod(z.size()[1:])
         ll = prior_ll + sldj
         nll = -ll.mean()
-
         return nll
 
 # calculates mean of values over every dimension of the tensor if there are multiple dimensions
@@ -61,21 +61,19 @@ def mean_over_dimensions(tensor, dim=None, keepdims=False):
                 tensor.squeeze_(d-i)
         return tensor
 
-# squeezing and unsqueezing (streaching) the model
+# squeezing and unsqueezing (streaching)
 def squeeze(x, reverse=False):
     b, c, h, w = x.size()
-
-    if reverse:
-        # unsqueeze
-        x = x.view(b, c // 4, 2, 2, h, w)
-        x = x.permute(0, 1, 4, 2, 5, 3).contiguous()
-        x = x.view(b, c // 4, h * 2, w * 2)
-    else:
+    if not reverse:
         # squeeze
         x = x.view(b, c, h //2, 2, w //2, 2)
         x = x.permute(0, 1, 3, 5, 2, 4).contiguous()
         x = x.view(b, c * 2 * 2, h // 2, w // 2)
-    
+    else:
+        # unsqueeze
+        x = x.view(b, c // 4, 2, 2, h, w)
+        x = x.permute(0, 1, 4, 2, 5, 3).contiguous()
+        x = x.view(b, c // 4, h * 2, w * 2)
     return x
 
 # clipping gradient norm to avoid exploding gradients
@@ -95,7 +93,6 @@ def sample(model, batch_size, device):
     # use the invertibility principle to get the sample
     x, _ = model(z, reverse=True)
     x = torch.sigmoid(x)
-
     return x
 
 def save_model_checkpoint(model, epoch, avg_loss, best=False):
@@ -111,17 +108,18 @@ def save_model_checkpoint(model, epoch, avg_loss, best=False):
                 'test_loss': avg_loss}, saving_path)
     print("model saved to a file named {}".format(file_name))
 
-def split_tensor(tensor, op_type="split"):
-    tensor_size_1 = tensor.size(1)
-    if op_type == "split":
-        return tensor[:, :tensor_size_1 // 2, ...], tensor[:, tensor_size_1 // 2:, ...]
-    elif op_type == "cross":
-        return tensor[:, 0::2, ...], tensor[:, 1::2, ...]
-    else:
-        print("Incorrect operation type!")
-        return 0
-
 def loss(density, zk, log_jacobians):
     sum_of_log_jacobians = sum(log_jacobians)
     return (-sum_of_log_jacobians - torch.log(density(zk)+1e-9)).mean()
 
+# just some calculations for forward split layer
+def gaussian_log(x, mean, logsd):
+    return -0.5 * math.log(2 * math.pi) - logsd - 0.5 * (x - mean) ** 2 / torch.exp(2 * logsd)
+
+# calculation for rever split layer
+def gaussian_sample(mean, logsd, eps=1):
+    return mean + torch.exp(logsd) * eps
+
+def gaussian_likelihood(x, logs, mean):
+    gaussian_p = gaussian_log(x, mean, logs)
+    return torch.sum(gaussian_p, dim=[1,2,3])
