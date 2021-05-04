5c99c107dc53a81e1d81b28d1670e6659a30d9a2
juliawiktoria <maybe.nott123@gmail.com>
2021-05-04 01:10:24 +0100

    updated main flow of the program and cleanup

--- main.py
+++ main.py
@@ -20,72 +20,79 @@ from datasets import *
 from flows import *
 
 # enablig grad for loss calc
-@torch.enable_grad()
-def train(epoch, model, trainloader, device, optimizer, scheduler, loss_func, max_grad_norm):
+# @torch.no_grad()
+def train(epoch, model, trainloader, device, optimizer, scheduler, loss_func, max_grad_norm, global_step):
     print("===> EPOCH {}".format(epoch))
-    global global_step
+    # global global_step
     # initialising training mode; just so the model "knows" it is training
     model.train()
+    # initialising counter for loss calculations
     loss_meter = AvgMeter()
-    
-    # training on batches
+
+    # with tqdm(total=len(trainloader.dataset)) as progress_bar:
     for x, _ in trainloader:
         x = x.to(device)
         optimizer.zero_grad()
         # forward pass so reverse mode is turned off
-        z, sum_lower_det_jacobian = model(x, reverse=False)
+        z, sldj = model(x, reverse=False)
         # calculating and updating loss
-        loss = loss_func(z, sum_lower_det_jacobian)
+        loss = loss_func(z, sldj)
         loss_meter.update(loss.item(), x.size(0))
         loss.backward()
 
-        # clip gradient if too much
-        if max_grad_norm > 0:
-            clip_grad_norm(optimizer, max_grad_norm)
+        # # clip gradient if too much
+        # if max_grad_norm > 0:
+        #     clip_grad_norm(optimizer, max_grad_norm)
 
-        # advance optimizer and scheduler 
+        # advance optimizer and scheduler and update parameters
         optimizer.step()
         scheduler.step()
+            # progress_bar.set_postfix(nll=loss_meter.avg,
+            #                         bpd=bits_per_dimension(z, loss_meter.avg),
+            #                         lr=optimizer.param_groups[0]['lr'])
+            # progress_bar.update(x.size(0))
 
         global_step += x.size(0)
+    # print('output dimensions: {}'.format(z.size()))
+    return global_step
 
 @torch.no_grad()
-def test(epoch, model, testloader, device, loss_func, num_samples, args):
-    print("testing func")
-    global best_loss
+def test(epoch, model, testloader, device, optimizer, scheduler, loss_func, best_loss, args):
+    print("\n====testing func====")
+    # setting a flag for indicating if this epoch is best ever
+    best = False
 
     model.eval()
     loss_meter = AvgMeter()
 
     for x, _ in testloader:
         x = x.to(device)
-        z, sum_lower_det_jacobian = model(x, reverse=False)
-        loss = loss_func(z, sum_lower_det_jacobian)
+        # print('x size in testing function: {}'.format(x.size()))
+        z, sldj = model(x, reverse=False)
+        loss = loss_func(z, sldj)
         loss_meter.update(loss.item(), x.size(0))
 
-    # checking if a checpoint should be saves
+    if loss_meter.avg < best_loss:
+        print('Updating best loss: [{}] -> [{}]'.format(best_loss, loss_meter.avg))
+        best_loss = loss_meter.avg
+        # indicating this epoch has achieved the best loss value
+        best = True
+        
     if epoch % args.ckpt_interval == 0:
         print('Saving checkpoint file from the epoch #{}'.format(epoch))
+        save_model_checkpoint(model, epoch, optimizer, scheduler, loss_meter.avg, best)
 
-    # checking if this current epoch is a saving interval
+    # Save samples and data on the specified interval
     if epoch % args.img_interval == 0:
         print("saving images from the epoch #{}".format(epoch))
-        images = sample(model, num_samples, device) 
+        images = sample(model, device, args)
         path_to_images = 'samples/epoch_' + str(epoch) # custom name for each epoch
-        os.makedirs(path_to_images, exist_ok=True) # create a dir for each epoch
-
-        # save every image separately with an unique name
-        for i in range(images.size(0)):
-            torchvision.utils.save_image(images[i, :, :, :], '{}/img_{}.png'.format(path_to_images, i))
-
-        # check if this epoch a grid should be saved
-        if epoch % args.grid_interval == 0:
-            print('saving nice grid')
-            # save a grid of images
-            images_concat = torchvision.utils.make_grid(images, nrow=int(num_samples ** 0.5), padding=2, pad_value=255)
-            torchvision.utils.save_image(images_concat, 'samples/grid_epoch_{}.png'.format(epoch))
+        save_grid = epoch % args.grid_interval == 0
+        save_sampled_images(epoch, images, path_to_images, if_grid=save_grid)
+    return best_loss
 
 if __name__ == '__main__':
+
     # parsing args for easier running of the program
     parser = argparse.ArgumentParser()
     
@@ -120,6 +127,9 @@ if __name__ == '__main__':
     best_loss = 0
     global_step = 0
 
+    # create directory for checkpoint files 
+    # os.makedirs('checkpoints', exist_ok=True)
+    
     # training on GPU if possible
     device = 'cuda' if torch.cuda.is_available() and args.gpu else 'cpu'
 
@@ -128,8 +138,13 @@ if __name__ == '__main__':
     
     # define the model
     if args.model == 'glow':
-        model = GlowModel(4*3, args.num_channels, args.num_levels, args.num_steps)
+        model = GlowModel(args.num_channels, args.num_levels, args.num_steps)
         model = model.to(device)
+
+    # optimizer takes care of updating the parameters of the model after each batch
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
+    # scheduler takes care of the adjustment of the learning rate
+    scheduler = sched.LambdaLR(optimizer, lambda s: min(1., s / args.sched_warmup))
 
     # account for training continuation; if incorrect checkpoint file name terminate the program with an appropriate error message
     if args.resume_training:
@@ -144,36 +159,53 @@ if __name__ == '__main__':
         else:
             checkpoint_loaded = torch.load(args.ckpt_path)
             model.load_state_dict(checkpoint_loaded['state_dict'])
-            # global best_loss
-            # global global_step
+            optimizer.load_state_dict(checkpoint_loaded['optim'])
+            scheduler.load_state_dict(checkpoint_loaded['sched'])
             best_loss = checkpoint_loaded['test_loss']
             starting_epoch = checkpoint_loaded['epoch']
             global_step = starting_epoch * len(trainset)
         print("resuming training from checkpoint file: {}".format(args.ckpt_path))
+    else:
+        # if training from scratch then init default values
+        global_step = 0
+        best_loss = 0
+        starting_epoch = 1
 
-    # defining the loss function, the optimizer, and the scheduler
-    # kinda established things
-    loss_function = NLLLoss().to(device)
-    optimizer = optim.Adam(model.parameters(), lr=args.lr)
-    scheduler = sched.LambdaLR(optimizer, lambda s: min(1., s / args.sched_warmup))
+    # run in training mode
+    if args.usage_mode == 'train':
+        # defining the loss function, the optimizer, and the scheduler
+        # kinda established things
+        loss_function = NLLLoss().to(device)
 
-    # for reference
-    times_array = []
+        # for reference
+        times_array = []
 
-    # training loop repeating for a specified number of epochs; starts from #1 in order to start naming epochs from 1
-    print("Starting training of the Glow model")
-    for epoch in range(1, args.epochs + 1):
-        print("Epoch [{} / {}]".format(epoch, args.epochs))
+        # training loop repeating for a specified number of epochs; starts from #1 in order to start naming epochs from 1
+        print("Starting training of the Glow model")
+        for epoch in range(starting_epoch, args.epochs + 1):
+            print("Epoch [{} / {}]".format(epoch, args.epochs))
 
-        # measuring epoch execution time for reference
-        start_time = time.time()
+            # measuring epoch execution time for reference
+            start_time = time.time()
 
-        # each epoch consist of training part and testing part
-        train(epoch, model, trainloader, device, optimizer, scheduler, loss_function, args.grad_norm)
-        test(epoch, model, testloader, device, loss_function, args.num_samples, args)
+            # each epoch consist of training part and testing part
+            new_global_step = train(epoch, model, trainloader, device, optimizer, scheduler, loss_function, global_step, args.grad_norm)
+            new_best_loss = test(epoch, model, testloader, device, optimizer, scheduler, loss_function, best_loss, args)
 
-        elapsed_time = time.time() - start_time
-        # recording time per epoch to a dataframe
-        times_array.append(["Epoch " + str(epoch) + ": ", time.strftime("%H:%M:%S", time.gmtime(elapsed_time))])
-    
-    print("the training is finished.")
+            global_step, best_loss = new_global_step, new_best_loss
+
+            elapsed_time = time.time() - start_time
+            # recording time per epoch to a dataframe
+            times_array.append(["Epoch " + str(epoch) + ": ", time.strftime("%H:%M:%S", time.gmtime(elapsed_time))])
+        
+        print("the training is finished.")
+    # run in sampling mode
+    elif args.usage_mode == 'sample':
+        print('sampling')
+        images = sample(model, device, args)
+        path_to_images = 'samples/epoch_' + str(starting_epoch) # custom name for each epoch
+        save_sampled_images(starting_epoch, images, args.num_samples, path_to_images, if_grid=True)
+    # incorrect mode
+    else:
+        model.describe_self()
+        sys.exit('Incorrect usage mode! Try --usage_mode train/sample')
