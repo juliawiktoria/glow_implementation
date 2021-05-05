d8033d2417fe4f16d86448539b57d0fd5be9638b
juliawiktoria <maybe.nott123@gmail.com>
2021-05-05 15:12:37 +0100

    added some changes from the notebook

--- glow_model.py
+++ glow_model.py
@@ -11,17 +11,16 @@ from model_parts import *
 # class for building GlowModel, not to be used on its own
 class _FlowStep(nn.Module):
     # comprises of three / four transforms
-    def __init__(self, in_channels, mid_channels, step_num):
+    def __init__(self, num_features, hid_layers, step_num):
         super(_FlowStep, self).__init__()
-
-        print('initialising flow step with {} in channels.'.format(in_channels))
+        # step ID for description and reference
         self.step_id = step_num
 
         # define transforms; hardcoded, not a framework for creating own models with different transforms
-        self.normalisation = ActivationNormalisation(in_channels)
-        self.convolution = InvertedConvolution(in_channels)
+        self.normalisation = ActivationNormalisation(num_features)
+        self.convolution = InvertedConvolution(num_features)
         self.flow_transformation = None
-        self.coupling = AffineCoupling(in_channels // 2, mid_channels)
+        self.coupling = AffineCoupling(num_features // 2, hid_layers)
 
     def describe(self):
         print('\t\t - > STEP {}'.format(self.step_id))
@@ -49,17 +48,18 @@ class _FlowStep(nn.Module):
 class _GlowLevel(nn.Module):
     # creates one glow level
     # level comprises of a squeeze step, K flow steps, and split step (except for the last leves, which does not have a split step)
-    def __init__(self, in_channels, mid_channels, num_steps, lvl_num, if_split=True):
+    def __init__(self, num_features, hid_layers, num_steps, lvl_num, if_split=True):
         super(_GlowLevel, self).__init__()
+        # lvl ID for description and reference
         self.lvl_id = lvl_num
         # squeeze operation
         self.squeeze = Squeeze()
         # create K steps of the flow K x ([t,t,t]) where t is a flow transform
-        # channels are multiplied by 4 to account for squeeze operation that takes place before flow steps
-        self.steps = nn.ModuleList([_FlowStep(in_channels=in_channels * 4, mid_channels=mid_channels, step_num=i+1) for i in range(num_steps)])
+        # channels (features) are multiplied by 4 to account for squeeze operation that takes place before flow steps
+        self.steps = nn.ModuleList([_FlowStep(in_channels=num_features * 4, hid_layers=hid_layers, step_num=i+1) for i in range(num_steps)])
         # split operation is not performed in the last forward level (in the first when reversed)
         self.if_split = if_split
-        self.split = Split2d(in_channels * 4)
+        self.split = Split2d(num_features * 4)
 
     def describe(self):
         print('\t - > Level {}'.format(self.lvl_id))
@@ -106,17 +106,21 @@ class _GlowLevel(nn.Module):
 
 # the whole model
 class GlowModel(nn.Module):
-    def __init__(self, num_channels, num_levels, num_steps):
+    def __init__(self, num_features, hid_layers, num_levels, num_steps, img_height, img_width):
         super(GlowModel, self).__init__()
-        self.in_channels = 3
-        self.num_channels = num_channels
+        self.num_features = num_features
+        self.hid_layers = hid_layers
         self.num_levels = num_levels
         self.num_steps = num_steps
-        self.in_height = 32
-        self.in_width = 32
+
+        # hardcoded for cifar10
+        self.in_height = img_height
+        self.in_width = img_width
+
         self.register_buffer('bounds', torch.tensor([0.9], dtype=torch.float32))
 
-        self.out_channels = self.in_channels * (2 ** (num_levels + 1))
+        # computes the size of the output tensor so it can be used to generate input for the reverse model
+        self.out_channels = self.num_features * (2 ** (num_levels + 1))
         self.out_height = int(self.in_height // (2 ** (num_levels)))
         self.out_width = int(self.in_width // (2 ** (num_levels)))
 
@@ -124,6 +128,8 @@ class GlowModel(nn.Module):
         self.create_levels()
 
     def describe(self):
+        # method for describing the rchitecture of the model, when called it calls all its subparts
+        # and produces a nice visualisation of the levels, steps, and transforms
         print('==============GLOW MODEL============')
         for level in self.levels:
             level.describe()
@@ -133,10 +139,10 @@ class GlowModel(nn.Module):
         # creates nn.ModuleList of all levels of the flow`
         # first (L - 1) levels include splitting
         for i in range(self.num_levels - 1):
-            self.levels.append(_GlowLevel(in_channels=self.in_channels, mid_channels=self.num_channels, num_steps=self.num_steps, lvl_num=i+1))
+            self.levels.append(_GlowLevel(in_channels=self.num_features, mid_channels=self.hid_layers, num_steps=self.num_steps, lvl_num=i+1))
             self.in_channels *= 2
         # last layer without the split part
-        self.levels.append(_GlowLevel(in_channels=self.in_channels, mid_channels=self.num_channels, num_steps=self.num_steps, lvl_num=self.num_levels, if_split=False))
+        self.levels.append(_GlowLevel(in_channels=self.num_features, mid_channels=self.hid_layers, num_steps=self.num_steps, lvl_num=self.num_levels, if_split=False))
     
     def _pre_process(self, x):
         # pre-process input
@@ -153,20 +159,20 @@ class GlowModel(nn.Module):
     def forward(self, x, reverse=False, temp=None):
         # defining first log_det for the forward pass
         if not reverse:
-            # print('\t\t-> model forward')
             if x.min() < 0 or x.max() > 1:
                 raise ValueError('Expected x in [0, 1], got min/max [{}, {}]'.format(x.min(), x.max()))
+           
             x, log_det_jacobian = self._pre_process(x)
-        # defining first log_det for thereverse pass
+            # go through all the levels iteratively
+            for level in self.levels:
+                x, log_det_jacobian = level(x, log_det_jacobian, reverse, temp)
         else:
-            # print('\t\t-> model reverse') 
-            log_det_jacobian = torch.zeros(x.size(0), device=x.device)
             # reverse the ordering of the levels do the no-split level is the first one now
             self.reversed_levels = self.levels[::-1]
-        # pass the input through all the glow levels iteratively
-        # each block solves the direction of the pass within itself
-        for level in self.levels:
-            x, log_det_jacobian = level(x, log_det_jacobian, reverse, temp)
-            # level_number += 1
-        # print('after all levels x size:{}\tlog size: {}'.format(x.size(), log_det_jacobian.size()))
+
+            # generate log det jacobian for reverse pass, input x should be passed to the function itself from sampling functions
+            log_det_jacobian = torch.zeros(x.size(0), device=x.device)
+            for level in self.reversed_levels:
+                x, log_det_jacobian = level(x, log_det_jacobian, reverse, temp)
+
         return x, log_det_jacobian
